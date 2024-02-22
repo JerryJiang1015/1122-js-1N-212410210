46070ae7d533dabdcffc84e2c5cc6108dc2da149
“JerryJiang1015” <212410210@o365.tku.edu.tw>
2024-02-22 21:14:20 +0800

    W01-P3: implement subtract function

---
+++ demo/w01_dom_10/js/w01_10.js
@@ -0,0 +1,67 @@
+const userInput = document.querySelector('#input-number');
+const addBtn = document.querySelector('#btn-add');
+const subtractBtn = document.querySelector('#btn-subtract');
+const multiplyBtn = document.querySelector('#btn-multiply');
+const divideBtn = document.querySelector('#btn-divide');
+const currentCalculationOutput = document.querySelector('#current-calculation');
+const currentResultOutput = document.querySelector('#current-result');
+
+// console.log('userInput', userInput);
+// console.log('currentCalculation', currentCalculation);
+// console.log('currentResult', currentResult);
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
+function outputResult(result, text) {
+  currentCalculationOutput.textContent = text;
+  currentResultOutput.textContent = result;
+}
+
+function add() {
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  //   console.log(operand1, operand2, operand1 + operand2);
+  currentResult = operand1 + operand2;
+  const calText = `${operand1} + ${operand2}`;
+  console.log(`${operand1} + ${operand2} = ${currentResult}`);
+  outputResult(currentResult, calText);
+}
+
+function subtract() {
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  //   console.log(operand1, operand2, operand1 - operand2);
+  currentResult = operand1 - operand2;
+  const calText = `${operand1} - ${operand2}`;
+  console.log(`${operand1} - ${operand2} = ${currentResult}`);
+  outputResult(currentResult, calText);
+}
+
+function multiply() {
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  // console.log(operand1, operand2, operand1 * operand2);
+  currentResult = operand1 * operand2;
+  const calText = `${operand1} * ${operand2}`;
+  console.log(`${operand1} * ${operand2} = ${currentResult}`);
+  outputResult(currentResult, calText);
+}
+
+function divide() {
+  const operand1 = currentResult;
+  const operand2 = getUserInput();
+  // console.log(operand1, operand2, operand1 / operand2);
+  currentResult = operand1 / operand2;
+  const calText = `${operand1} / ${operand2}`;
+  console.log(`${operand1} / ${operand2} = ${currentResult}`);
+  outputResult(currentResult, calText);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
