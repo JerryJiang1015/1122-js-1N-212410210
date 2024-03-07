0225f69d474fe36796c7de4d385296b184c5dbe2
“JerryJiang1015” <212410210@o365.tku.edu.tw>
2024-03-07 20:54:45 +0800

    ## W03-P4: Compute highest and lowest score of sdata and students2

--- demo/w03_basics_10/p3_10/p3_10.js
+++ demo/w03_basics_10/p3_10/p3_10.js
@@ -13,6 +13,7 @@ console.log('sdata after sorting descending', sdata);
 console.log(`The highest score: ${sdata[0]}`);
 console.log(`The lowest score: ${sdata[sdata.length - 1]}`);
 
+console.log('students', students);
 const students2 = students.map((student) => {
   return { ...student, role: 'student' };
 });
@@ -21,3 +22,7 @@ console.log('students unsorted', students2);
 students2.sort(function (a, b) {
   return b.score - a.score;
 });
+
+console.log('students2 after sorting descending', students2);
+console.log(`The highest score: ${students2[0].score}`);
+console.log(`The lowest score: ${students2[students2.length - 1].score}`);
