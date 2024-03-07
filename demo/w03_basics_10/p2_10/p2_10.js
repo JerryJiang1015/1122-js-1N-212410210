fdc12cab29ce1393fdc9e865f5d80bb3af4c6ed5
“JerryJiang1015” <212410210@o365.tku.edu.tw>
2024-03-07 20:41:33 +0800

    W03-P2: P2_xx temperature convert using Web

--- demo/w03_basics_10/p2_10/p2_10.js
+++ demo/w03_basics_10/p2_10/p2_10.js
@@ -1,44 +1,24 @@
 const userInput = document.querySelector('#input-number');
 const cBtn = document.querySelector('#btn-c');
 const fBtn = document.querySelector('#btn-f');
-const currentCalculationOutput = document.querySelector('#current-calculation');
-const currentResultOutput = document.querySelector('#current-result');
 
-const defaultResult = 0;
-let currentResult = defaultResult;
-
-function getUserInput() {
-  return parseFloat(userInput.value);
-}
-
-function outputResult(result, text) {
-  currentCalculationOutput.textContent = text;
-  currentResultOutput.textContent = result;
-}
-
-function F() {
-  const b = parseFloat(getUserInput());
-
-  const c = ((b - 32) * 5) / 9;
-
-  outputResult(b, c);
-}
-
-function b() {
-  const f = parseFloat(userInput.value);
-  const c = ((f - 32) * 5) / 9;
-  const output = `${f} F = ${celsius.toFixed(2)} C`;
+function C() {
+  const c = parseFloat(userInput.value);
+  const f = (c * 9) / 5 + 32;
+  const output = `${c} C = ${f.toFixed(2)} F`;
   displayResult(output);
 }
 
-function C() {
-  const operand1 = currentResult;
-  const b = parseFloat(getUserInput());
+function F() {
+  const f = parseFloat(userInput.value);
+  const c = ((f - 32) * 5) / 9;
+  const output = `${f} F = ${c.toFixed(2)} C`;
+  displayResult(output);
+}
 
-  currentResult = operand1 - b;
-  const calText = `${operand1} - ${b}`;
-  console.log(`${operand1} - ${b} = ${currentResult}`);
-  outputResult(currentResult, calText);
+function displayResult(output) {
+  const resultElement = document.querySelector('#results');
+  resultElement.innerHTML = `<h1 id="current-calculation">${output}</h1>`;
 }
 
 cBtn.addEventListener('click', C);
--- demo/w03_basics_10/p3_10/p3_10.js
+++ demo/w03_basics_10/p3_10/p3_10.js
@@ -2,3 +2,22 @@ import { sdata, students } from './data_10.js';
 
 console.log('sdata', sdata);
 console.log('students', students);
+
+sdata.sort();
+console.log('sdata after sorting alphabetically', sdata);
+
+sdata.sort(function (a, b) {
+  return b - a;
+});
+console.log('sdata after sorting descending', sdata);
+console.log(`The highest score: ${sdata[0]}`);
+console.log(`The lowest score: ${sdata[sdata.length - 1]}`);
+
+const students2 = students.map((student) => {
+  return { ...student, role: 'student' };
+});
+
+console.log('students unsorted', students2);
+students2.sort(function (a, b) {
+  return b.score - a.score;
+});
