7e5698f6b708470c67f5d0005bc1a84031840f10
“JerryJiang1015” <212410210@o365.tku.edu.tw>
2024-03-28 22:03:03 +0800

    W06-P4: implement open modals

---
+++ demo/w06_modal_10_/script_10.js
@@ -0,0 +1,40 @@
+const modals = document.querySelectorAll('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModals = document.querySelectorAll('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = (modalId) => {
+  const pModal = document.querySelector(`.${modalId}`);
+  console.log('pModal', pModal);
+  pModal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = (modalId) => {
+  const pModal = document.querySelector(`.${modalId}`);
+  pModal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const closeAllModals = () => {
+  modals.forEach((modal) => {
+    modal.classList.add('hidden');
+  });
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const modalId = e.currentTarget.dataset.id;
+    console.log('modalId', modalId);
+    openModal(modalId);
+  });
+});
+
+btnCloseModals.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    closeModal(e.currentTarget.closest('.modal').dataset.id);
+  });
+});
+
+overlay.addEventListener('click', closeAllModals);
