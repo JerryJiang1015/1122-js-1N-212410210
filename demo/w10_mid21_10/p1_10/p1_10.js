d8287d14bc1cd81910964627896ea7e66e735251
“JerryJiang1015” <212410210@o365.tku.edu.tw>
2024-04-25 21:01:28 +0800

    W10-P3: Get 9 products data from Supabase, and show in console

--- demo/w10_mid21_10/p1_10/p1_10.js
+++ demo/w10_mid21_10/p1_10/p1_10.js
@@ -1,11 +1,13 @@
-import { products_xx, all_products_xx } from './p1_data_10.js';
+// import { products_10, all_products_10 } from './p1_data_10.js';
+
+let product_10 = [];
 
 const productContainer = document.querySelector('.products-container');
 
-console.log('products_xx', products_xx);
+console.log('products_10', products_10);
 
 const DisplayProducts = (products) => {};
 
 document.addEventListener('DOMContentLoaded', () => {
-  DisplayProducts(products_xx);
+  DisplayProducts(products_10);
 });
--- demo/w10_mid21_10/p1_10/product_10.sql
+++ demo/w10_mid21_10/p1_10/product_10.sql
@@ -7,7 +7,7 @@ CREATE TABLE product_10 (
     remote_img varchar(255)
 )
 
-INSERT INTO product_10 (id, title, price, category, img, remote_img)
+INSERT INTO product_10 (id, title, price, category, img, remote_url)
 VALUES
 (1,'Emperor Bed', 21.99,'Liddy','./images/product-1.jpg',''),
 (2,'Accent Chair', 25.99,'Caressa','./images/product-2.jpg',''),
@@ -18,11 +18,11 @@ VALUES
 (7,'Albany Sectional', 10.99,'Ikea','./images/product-6.jpg',''),
 (8,'Sofa Set', 69.99,'Liddy','./images/product-8.jpg','')
 
-INSERT INTO product_10 (id, title, price, category, img, remote_img)
+INSERT INTO product_10 (id, title, price, category, img, remote_url)
 VALUES
-(13,'Wooden Table', 19.99,'Ikea','./images/product-13.jpg','https://unsplash.com/s/photos/table')
+(13,'Wooden Table', 19.99,'Ikea','./images/product-20.jpg','https://unsplash.com/s/photos/table')
 
-INSERT INTO product_10 (id, title, price, category, img, remote_img)
+INSERT INTO product_10 (id, title, price, category, img, remote_url)
 VALUES
 (1,'Emperor Bed', 21.99,'Liddy','./images/product-1.jpg',''),
 (2,'Accent Chair', 25.99,'Caressa','./images/product-2.jpg',''),
@@ -36,4 +36,4 @@ VALUES
 (10,'Modern Bookshelf', 8.99,'Marcos','./images/product-10.jpg',''),
 (11,'Albany Table', 79.99,'Marcos','./images/product-11.jpg',''),
 (12,'Leather Sofa', 39.99,'Liddy','./images/product-12.jpg',''),
-(13,'Wooden Table', 19.99,'Ikea','./images/product-13.jpg','')
+(13,'Wooden Table', 19.99,'Ikea','./images/product-20.jpg','https://pse.is/5v9e6d')
