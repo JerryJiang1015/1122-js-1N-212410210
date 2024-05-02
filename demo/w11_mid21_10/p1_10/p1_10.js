c29794636f1ecf9d7687f1c05b701f57a8a25d6b
“JerryJiang1015” <212410210@o365.tku.edu.tw>
2024-05-02 19:11:07 +0800

    W11-P1: Answer p2_xx in mid21

---
+++ demo/w11_mid21_10/p1_10/p1_10.js
@@ -0,0 +1,13 @@
+// import { products_10, all_products_10 } from './p1_data_10.js';
+
+let product_10 = [];
+
+const productContainer = document.querySelector('.products-container');
+
+console.log('products_10', products_10);
+
+const DisplayProducts = (products) => {};
+
+document.addEventListener('DOMContentLoaded', () => {
+  DisplayProducts(products_10);
+});
