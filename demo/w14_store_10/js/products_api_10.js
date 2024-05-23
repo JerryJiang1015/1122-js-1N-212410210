71baef20a0a417148a0fffaf115d16624cbeb152
“JerryJiang1015” <212410210@o365.tku.edu.tw>
2024-05-23 19:22:19 +0800

    W14-P1: Do products_api_10.js to get data from an api url

---
+++ demo/w14_store_10/js/products_api_10.js
@@ -0,0 +1,47 @@
+// import { products_10, all_products_10 } from './p1_data_10.js';
+
+import { _supabase } from './clientSupabase_10.js';
+
+const url = 'https://www.course-api.com/javascript-store-products'
+
+let products_10 = [];
+
+const fetchData = async (url) => {
+  try {
+    const reponse = await fetch(url);
+    const data = await reponse.json();
+    console.log('fetchData', data);
+    return data ;
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+const productContainer = document.querySelector('.products-container');
+
+console.log('products_10', products_10);
+
+const displayProducts = (products) => {
+  let productsContent = products.map((product) => {
+    const { name,  price, image } = product.fields;
+    return `
+ <div class="single-product">
+ <img
+          src="${image[0].url}"
+          class="single-product-img img"
+          alt="${name}"
+        />
+ <footer>
+ <h3 class="name">${name}</h3>
+ <span class="price">${price}</span>
+ </footer>
+ </div>
+    `;
+  }).join('');
+  productContainer.innerHTML = productsContent;
+ };
+
+document.addEventListener('DOMContentLoaded', async () => {
+  products_10 = await fetchData(url);
+  displayProducts(products_10);
+});
---
+++ demo/w14_store_10/js/products_localjson_10.js
@@ -0,0 +1,46 @@
+// import { products_10, all_products_10 } from './p1_data_10.js';
+
+import { _supabase } from './clientSupabase_10.js';
+
+let products_10 = [];
+
+const getProductsSupabase_10 = async () => {
+  try {
+    let { data, error } = await _supabase.from('product_10').select('*');
+    console.log('product data', data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const productContainer = document.querySelector('.products-container');
+
+console.log('products_10', products_10);
+
+const displayProducts = (products) => {
+  let productsContent = products
+    .map((product) => {
+      const { id, title, price, category, img, remote_url } = product;
+      return `
+        <div class="single-product">
+        <img
+          src=${img}
+          class="single-product-img img"
+          alt=${title}
+        />
+        <footer>
+          <h3 class="name">${title} (${id})</h3>
+          <span class="price">$${price}</span>
+        </footer>
+      </div>
+    `;
+    })
+    .join('');
+  productContainer.innerHTML = productsContent;
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  products_10 = await getProductsSupabase_10();
+  displayProducts(products_10);
+});
