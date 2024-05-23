4557496b93f25e4c298b5a77da0a30998af230c0
“JerryJiang1015” <212410210@o365.tku.edu.tw>
2024-05-23 21:13:55 +0800

    W14-P4: Do products_supa_10.js to get data from Supabase

--- demo/w14_store_10/js/products_supa_10.js
+++ demo/w14_store_10/js/products_supa_10.js
@@ -1,13 +1,13 @@
 // import { products_10, all_products_10 } from './p1_data_10.js';
 
-import { _supabase } from './clientSupabase_10';
+import { _supabase } from './clientSupabase_10.js';
 
 let products_10 = [];
 
 const getProductsSupabase_10 = async () => {
   try {
-    let { data, error } = await _supabase.from('product_10').select('*');
-    console.log('product data', data);
+    let { data, error } = await _supabase.from('products_10').select('*');
+    console.log('products data', data);
     return data;
   } catch (error) {
     console.log(error);
@@ -21,16 +21,16 @@ console.log('products_10', products_10);
 const displayProducts = (products) => {
   let productsContent = products
     .map((product) => {
-      const { id, title, price, category, img, remote_url } = product;
+      const {title, price,localImg} = product;
       return `
         <div class="single-product">
         <img
-          src=${img}
+          src=${localImg}
           class="single-product-img img"
           alt=${title}
         />
         <footer>
-          <h3 class="name">${title} (${id})</h3>
+          <h3 class="name">${title}</h3>
           <span class="price">$${price}</span>
         </footer>
       </div>
